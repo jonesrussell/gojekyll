84a31fd80b921572df3b9f4d1c95136325db2f5f
Russell Jones <499552+jonesrussell@users.noreply.github.com>
2024-07-11 03:05:34 +0000

    Move meat to cmd

--- main.go
+++ main.go
@@ -1,55 +1,11 @@
 package main
 
 import (
-	"fmt"
-	"io/fs"
-	"log"
 	"os"
-	"path/filepath"
 
-	"github.com/rivo/tview"
+	"jonesrussell/jekyll-publisher/cmd"
 )
 
 func main() {
-	if len(os.Args) < 2 {
-		fmt.Println("Please provide the path to the Jekyll site as an argument.")
-		os.Exit(1)
-	}
-
-	sitePath := os.Args[1]
-	app := tview.NewApplication()
-
-	list := tview.NewList().
-		ShowSecondaryText(false).
-		SetHighlightFullLine(true)
-
-	list.AddItem("_drafts", "", 0, nil)
-	addFilenamesToList(sitePath, "_drafts", list)
-	list.AddItem("_posts", "", 0, nil)
-	addFilenamesToList(sitePath, "_posts", list)
-
-	list.SetSelectedFunc(func(i int, mainText string, secondaryText string, shortcut rune) {
-		app.Stop()
-	})
-
-	if err := app.SetRoot(list, true).Run(); err != nil {
-		log.Println("Could not set root")
-		panic(err)
-	}
-}
-
-func addFilenamesToList(sitePath string, dirName string, list *tview.List) {
-	dirPath := filepath.Join(sitePath, dirName)
-	err := filepath.WalkDir(dirPath, func(path string, d fs.DirEntry, err error) error {
-		if err != nil {
-			return err
-		}
-		if !d.IsDir() {
-			list.AddItem(filepath.Base(path), "", 0, nil)
-		}
-		return nil
-	})
-	if err != nil {
-		fmt.Printf("Error reading %s: %v\n", dirName, err)
-	}
+	cmd.Run(os.Args)
 }
